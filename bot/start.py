4d82d2bc8aa8690a5a050ca243ff5524b86306fc
SAF ONE <77989182+AsmSafone@users.noreply.github.com>
2021-09-05 19:08:53 +0600

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       await m.reply(f"**I am A advanced Anime Theme VC Video Player created for playing Video in the voice chats of Telegram Groups & Channels. \n\n**Type /help To View Comands:-** __ \n1) Type /info To View Devs`",   
+       await m.reply_text(f"**I am A advanced Anime Theme VC Video Player created for playing Video in the voice chats of Telegram Groups & Channels. \n\n**Type /help To View Comands:-** __ \n1) Type /info To View Devs`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[
                                      InlineKeyboardButton(
@@ -12,4 +12,4 @@ async def start(client, m: Message):
                                     ]]
                             ))
    else:
-      await m.reply("****")
+      await m.reply_text(f"**Hola, Join @AsmSafone!**")

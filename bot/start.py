4b41767610c2beeeda342ba6f5b8083d9d693c16
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-06 13:55:07 +0530

    my mad

--- bot/start.py
+++ bot/start.py
@@ -29,7 +29,7 @@ async def _human_time_duration(seconds):
 @Client.on_message(filters.command("start"))
 async def start(client, m: Message):
    if m.chat.type == 'private':
-       insert(int(message.chat.id))
+       insert(int(m.chat.id))
        await m.reply_text(f"**I am A advanced Anime Theme VC Video Player created for playing Video in the voice chats of Telegram Groups & Channels. \n\n**Type /help To View Comands:-** __ \n1) Type /info To View Devs`",   
                             reply_markup=InlineKeyboardMarkup(
                                 [[

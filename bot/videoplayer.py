81bf6cba44fd37a46e1015d13f49121d7d61602b
Shohih Abdul <shohih242@gmail.com>
2021-09-07 10:11:14 +0700

    Trying my best

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -6,10 +6,18 @@ from pytgcalls import GroupCallFactory
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import API_ID, API_HASH, SESSION_NAME
+from youtube_search import YoutubeSearch
+from pytube import YouTube
 
 app = Client(SESSION_NAME, API_ID, API_HASH)
 group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM)
 VIDEO_CALL = {}
+
+
+def video_link_getter(url: str):
+    yt = YouTube(url)
+    x = yt.streams.filter(file_extension="mp4", res="720p")[0].download("downloads")
+    return x
 
 
 @Client.on_message(filters.command("stream"))
@@ -19,44 +27,30 @@ async def stream(client, m: Message):
         if len(m.command) < 2:
             await m.reply("`Reply to some Video or Give Some Live Stream Url!`")
         else:
-            video = m.text.split(None, 1)[1]
+            video = " ".join(message.command[1:])
             youtube_regex = (
-                                         r'(https?://)?(www\.)?'
-                                       '(youtube|youtu|youtube-nocookie)\.(com|be)/'
-                                       '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
+                r'(https?://)?(www\.)?'
+                '(youtube|youtu|youtube-nocookie)\.(com|be)/'
+                '(watch\?v=|embed/|v/|.+\?v=)?([^&=%\?]{11})')
             youtube_regex_match = re.match(youtube_regex, video)
             if youtube_regex_match:
-            	try:
-            		yt = pafy.new(video)
-            		best = yt.getbest()
-            		video_url = best.url
-            	except Exception as e:
-            		await m.reply(f"**Error** -- `{e}`")
-            		return
-            	msg = await m.reply("`Starting Live Stream...`")
-            	chat_id = m.chat.id
-            	await asyncio.sleep(1)
-            	try:
-            	   group_call = group_call_factory.get_group_call()
-            	   await group_call.join(chat_id)
-            	   await group_call.start_video(video_url)
-            	   VIDEO_CALL[chat_id] = group_call
-            	   await msg.edit(f"**▶️ Started [Live Streaming](video) !**")
-            	except Exception as e:
-            		await msg.edit(f"**Error** -- `{e}`")
-            else:
-            	msg = await m.reply("`Starting Live Stream...`")
-            	chat_id = m.chat.id
-            	await asyncio.sleep(1)
-            	try:
-            	   group_call = group_call_factory.get_group_call()
-            	   await group_call.join(chat_id)
-            	   await group_call.start_video(video)
-            	   VIDEO_CALL[chat_id] = group_call
-            	   await msg.edit(f"**▶️ Started [Live Streaming](video) !**")
-            	except Exception as e:
-            		await msg.edit(f"**Error** -- `{e}`")
-            	
+                try:
+                    x = video_link_getter(video)
+                except Exception as e:
+                    await m.reply(f"**Error** -- `{e}`")
+                    return
+                msg = await m.reply("`Starting Live Stream...`")
+                chat_id = m.chat.id
+                await asyncio.sleep(1)
+                try:
+                    group_call = group_call_factory.get_group_call()
+                    await group_call.join(chat_id)
+                    await group_call.start_video(x)
+                    VIDEO_CALL[chat_id] = group_call
+                    await msg.edit(f"**▶️ Started [Live Streaming](video) !**")
+                except Exception as e:
+                    await msg.edit(f"**Error** -- `{e}`")
+
     elif replied.video or replied.document:
         msg = await m.reply("`Downloading...`")
         video = await client.download_media(m.reply_to_message)
@@ -72,6 +66,7 @@ async def stream(client, m: Message):
             await msg.edit(f"**🚫 Error** - `{e}`")
     else:
         await m.reply("`Reply to some Video!`")
+
 
 @Client.on_message(filters.command("stop"))
 async def stopvideo(client, m: Message):
